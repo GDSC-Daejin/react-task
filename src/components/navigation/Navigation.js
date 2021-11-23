b48343b9bd53fae4d6a7ab0c3e72ebecf060e649
HYEOK_E <jhjeong00@gmail.com>
2021-11-23 21:39:05 +0900

    fix : eslint rule 변경

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Navigation = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div
+        role="button"
+        onClick={() => {
+          navigate('/introduce');
+        }}
+      >
+        소개
+      </div>
+    </>
+  );
+};
+
+export default Navigation;
