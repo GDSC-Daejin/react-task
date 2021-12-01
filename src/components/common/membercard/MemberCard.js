a7a117d743dc29f100ee5fc930f0f973ff45f97f
dongseok sin <habaik0147@gmail.com>
2021-12-01 20:01:36 +0900

    feat:MemberCard수정

--- src/components/common/membercard/MemberCard.js
+++ src/components/common/membercard/MemberCard.js
@@ -6,20 +6,26 @@ import { memberList } from "../../../api/MemberList";
 export const MemberCard = () => {
   return (
     <>
-      <div className="StyledMemberCard">
-        {memberList.map((memberInfo, id) => (
-          <>
-            {memberInfo.memberImg ? (
-              <img className="MemberImg" src={memberInfo.memberImg} key={id} />
-            ) : (
-              <Skeleton variant={"circular"} height={200} width={200} />
-            )}
-            <div className="NickName">{memberInfo.nickName}</div>
-            <div className="Name">{memberInfo.name}</div>
-            <div className="Role">{memberInfo.role}</div>
-          </>
-        ))}
-      </div>
+      {memberList.map((memberInfo, id) => (
+        <>
+          <div className="MemberCardWrapper">
+            <div className="StyledMemberCard">
+              {memberInfo.memberImg ? (
+                <img
+                  className="MemberImg"
+                  src={memberInfo.memberImg}
+                  key={id}
+                />
+              ) : (
+                <Skeleton variant={"circular"} height={200} width={200} />
+              )}
+              <div className="NickName">{memberInfo.nickName}</div>
+              <div className="Name">{memberInfo.name}</div>
+              <div className="Role">{memberInfo.role}</div>
+            </div>
+          </div>
+        </>
+      ))}
     </>
   );
 };
